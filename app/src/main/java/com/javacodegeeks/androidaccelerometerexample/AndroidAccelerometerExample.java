7705115d1c32a2dc9bb56079e877d3979ebb4d23
Chase Lambert <theicfire@gmail.com>
2015-03-03 16:22:55 -0800

    New notification layout

--- app/src/main/java/com/javacodegeeks/androidaccelerometerexample/AndroidAccelerometerExample.java
+++ app/src/main/java/com/javacodegeeks/androidaccelerometerexample/AndroidAccelerometerExample.java
@@ -7,11 +7,14 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.Vibrator;
 import android.telephony.SmsManager;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -20,18 +23,19 @@ public class AndroidAccelerometerExample extends Activity implements SensorEvent
 	private SensorManager sensorManager;
 	private Sensor accelerometer;
 
-    private static final int MAX_NOTIFY_DELTA = 20 * 1000; // 20 seconds
-    private static final int MIN_NOTIFY_DELTA = 5 * 1000; // 20 seconds
-    private static final int TWO_MINUTES = 2 * 60 * 1000; // 20 seconds
+    private static final int MAX_NOTIFY_DELTA = 20 * 1000;
+    private static final int MIN_NOTIFY_DELTA = 5 * 1000;
+    private static final int TWO_MINUTES = 40 * 1000;
     public long last_notify = 0;
     private Queue<float[]> sensorEventQueue;
     public Queue<Long> movementTimesQueue; // TODO make private
 
 	private float vibrateThreshold = 0;
+    private Handler mHandler;
 
     private int count;
 
-	private TextView currentX, currentY, currentZ, countView;
+	private TextView countView, timeTillSMSAllowedView, notifyTimeRangeView;
 
 	public Vibrator v;
 
@@ -59,8 +63,51 @@ public class AndroidAccelerometerExample extends Activity implements SensorEvent
         count = 0;
         movementTimesQueue = new LinkedList<Long>();
 
-//        SmsManager.getDefault().sendTextMessage("5125778778", null, "this is a test message", null,null);
+        setupTimeUpdate();
 
+
+    }
+
+    private String timeTillSMSAllowed() {
+        if (System.currentTimeMillis() - last_notify > TWO_MINUTES) {
+            return "0";
+        }
+        return Long.toString((TWO_MINUTES - (System.currentTimeMillis() - last_notify)) / 1000);
+    }
+
+    private void updateNotifyTimeRangeView() {
+        if (shouldNotifyIfAdded(System.currentTimeMillis())) {
+            notifyTimeRangeView.setText("Notification on next bump!");
+        } else {
+            notifyTimeRangeView.setText("Notification are sleeping");
+        }
+    }
+
+    private void setupTimeUpdate() {
+        mHandler = new Handler();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        Thread.sleep(1000);
+                        mHandler.post(new Runnable() {
+
+                            @Override
+                            public void run() {
+                                Log.d("mine", "come very 1 second");
+                                timeTillSMSAllowedView.setText(timeTillSMSAllowed());
+                                updateNotifyTimeRangeView();
+
+
+                            }
+                        });
+                    } catch (Exception e) {
+                        Log.e("mine", "TODO something bad here, not sure what to do");
+                    }
+                }
+            }
+        }).start();
     }
 
     private float maxAccelDifference(float[] current) {
@@ -75,12 +122,12 @@ public class AndroidAccelerometerExample extends Activity implements SensorEvent
     }
 
 	public void initializeViews() {
-		currentX = (TextView) findViewById(R.id.currentX);
-		currentY = (TextView) findViewById(R.id.currentY);
-		currentZ = (TextView) findViewById(R.id.currentZ);
         countView = (TextView) findViewById(R.id.count);
+        timeTillSMSAllowedView = (TextView) findViewById(R.id.timeTillSMSAllowed);
+        notifyTimeRangeView = (TextView) findViewById(R.id.notifyTimeRange);
 
-	}
+
+    }
 
 	////onResume() register the accelerometer for listening the events
 	//protected void onResume() {
@@ -102,37 +149,49 @@ public class AndroidAccelerometerExample extends Activity implements SensorEvent
 	@Override
 	public void onSensorChanged(SensorEvent event) {
         count += 1;
+        countView.setText(Integer.toString(count));
         float[] current = {event.values[0], event.values[1], event.values[2]};
-        displayCurrentValues(current);
 		maybeVibrate(current);
 	}
 
     public boolean shouldNotify(long millis) {
         movementTimesQueue.add(millis);
+        if (shouldNotifyIfAdded(millis)) {
+            last_notify = millis;
+            return true;
+        }
+        return false;
+    }
 
+    public boolean shouldNotifyIfAdded(long millis) {
         // clear queue
         while (movementTimesQueue.peek() != null && movementTimesQueue.peek() < millis - MAX_NOTIFY_DELTA) {
             movementTimesQueue.remove();
         }
 
-        if (movementTimesQueue.peek() < millis - MIN_NOTIFY_DELTA) {
+        if (movementTimesQueue.peek() != null && movementTimesQueue.peek() < millis - MIN_NOTIFY_DELTA) {
             if (millis - last_notify > TWO_MINUTES) {
-                last_notify = millis;
                 return true;
             }
         }
         return false;
     }
 
-	// if the change in the accelerometer value is big enough, then vibrate!
+        // if the change in the accelerometer value is big enough, then vibrate!
 	// our threshold is MaxValue/2
 	public void maybeVibrate(float[] current) {
 
 
         if (maxAccelDifference(current) > vibrateThreshold) {
             if (shouldNotify(System.currentTimeMillis())) {
-                Log.d("mine", "Actual notify!");
+                Log.d("mine", "Actual notify. Sending sms!");
+                Date date = new Date();
+
+                SmsManager.getDefault().sendTextMessage("5125778778", null, "Phone moved -- " + date.toString(), null,null);
+                Toast.makeText(getApplicationContext(), "Sending SMS!", Toast.LENGTH_SHORT).show();
             }
+            updateNotifyTimeRangeView();
+
 //            v.vibrate(50);
             Log.d("mine", "Diff is great enough!");
             sensorEventQueue.clear();
@@ -143,14 +202,4 @@ public class AndroidAccelerometerExample extends Activity implements SensorEvent
         }
 
 	}
-	// display the current x,y,z accelerometer values
-	public void displayCurrentValues(float[] current) {
-
-		currentX.setText(Float.toString(current[0]));
-		currentY.setText(Float.toString(current[1]));
-		currentZ.setText(Float.toString(current[2]));
-
-        countView.setText(Integer.toString(count));
-	}
-
 }
