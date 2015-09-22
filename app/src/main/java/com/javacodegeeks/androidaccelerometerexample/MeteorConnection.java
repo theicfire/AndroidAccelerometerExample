6a68e4e5403d1fb85d9b69fb4ffb71bc782d3e51
Chase Lambert <chase@stripe.com>
2015-09-22 09:33:44 -0700

    System.out -> Log.d

--- app/src/main/java/com/javacodegeeks/androidaccelerometerexample/MeteorConnection.java
+++ app/src/main/java/com/javacodegeeks/androidaccelerometerexample/MeteorConnection.java
@@ -29,7 +29,7 @@ public class MeteorConnection implements MeteorCallback {
 
     @Override
     public void onConnect() {
-        System.out.println("Connected");
+        Log.d(TAG, "Connected");
         Log.d(TAG, "onConnect");
         reconnecting = false;
         if (!meteorSenderStarted) {
@@ -40,7 +40,7 @@ public class MeteorConnection implements MeteorCallback {
 
     @Override
     public void onDisconnect(int code, String reason) {
-        System.out.println("Disconnected");
+        Log.d(TAG, "Disconnected");
 
         if (!reconnecting) {
             Thread thread = new Thread(new Runnable() {
@@ -51,7 +51,7 @@ public class MeteorConnection implements MeteorCallback {
                     } catch (InterruptedException ex) {
                         Thread.currentThread().interrupt();
                     }
-                    System.out.println("Try to reconnect");
+                    Log.d(TAG, "Try to reconnect");
                     reconnecting = false;
                     mMeteor.reconnect();
                 }
@@ -64,25 +64,25 @@ public class MeteorConnection implements MeteorCallback {
 
     @Override
     public void onDataAdded(String collectionName, String documentID, String fieldsJson) {
-        System.out.println("Data added to <"+collectionName+"> in document <"+documentID+">");
-        System.out.println("    Added: "+fieldsJson);
+        Log.d(TAG, "Data added to <"+collectionName+"> in document <"+documentID+">");
+        Log.d(TAG, "    Added: "+fieldsJson);
     }
 
     @Override
     public void onDataChanged(String collectionName, String documentID, String updatedValuesJson, String removedValuesJson) {
-        System.out.println("Data changed in <"+collectionName+"> in document <"+documentID+">");
-        System.out.println("    Updated: "+updatedValuesJson);
-        System.out.println("    Removed: "+removedValuesJson);
+        Log.d(TAG, "Data changed in <"+collectionName+"> in document <"+documentID+">");
+        Log.d(TAG, "    Updated: "+updatedValuesJson);
+        Log.d(TAG, "    Removed: "+removedValuesJson);
     }
 
     @Override
     public void onDataRemoved(String collectionName, String documentID) {
-        System.out.println("Data removed from <"+collectionName+"> in document <"+documentID+">");
+        Log.d(TAG, "Data removed from <"+collectionName+"> in document <"+documentID+">");
     }
 
     @Override
     public void onException(Exception e) {
-        System.out.println("Exception");
+        Log.d(TAG, "Exception");
         if (e != null) {
             e.printStackTrace();
         }
